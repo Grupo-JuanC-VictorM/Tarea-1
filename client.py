98e1e96a675f8c067b3e614715d6297fa2cf37d6
vamorale <87721403+vamorale@users.noreply.github.com>
2023-04-09 20:17:59 -0400

    Encode/Decode functions

--- client.py
+++ client.py
@@ -1,5 +1,35 @@
 import socket
+import string
 
+# Alfabeto para decodificar/codificar:
+'''
+ ['{', 'C', 'g', 'r', '/', 'U', 'a', 'W', 'z', 'L', ',', '$', 'n', 'V', 'A', 'k', 'X', 
+ '`', 'w', ')', '(', 'J', '<', 'E', 'B', '_', '#', 'u', 'P', 'p', 'I', '[', 'M', ']', 
+ 'Q', '"', 'l', '|', 'T', '}', '!', 'j', 'm', '=', 'F', '\\', '+', 'o', '.', 'y', '-', 
+ 'G', 'q', '?', '>', ';', 'O', 'f', '*', ' ', 'S', 'R', ':', 't', '^', "'", 'b', 's', 
+ 'c', 'Z', 'd', 'x', '~', '@', 'K', 'h', 'N', 'D', 'e', '%', 'v', '&', 'H', 'i', 'Y']
+'''
+alphabet = "".join([" ",string.ascii_letters,string.punctuation])
+alphabet2=['{', 'C', 'g', 'r', '/', 'U', 'a', 'W', 'z', 'L', ',', '$', 'n', 'V', 'A', 'k', 'X', '`', 'w', ')', '(', 'J', '<', 'E', 'B', '_', '#', 'u', 'P', 'p', 'I', '[', 'M', ']', 'Q', '"', 'l', '|', 'T', '}', '!', 'j', 'm', '=', 'F', '\\', '+', 'o', '.', 'y', '-', 'G', 'q', '?', '>', ';', 'O', 'f', '*', ' ', 'S', 'R', ':', 't', '^', "'", 'b', 's', 'c', 'Z', 'd', 'x', '~', '@', 'K', 'h', 'N', 'D', 'e', '%', 'v', '&', 'H', 'i', 'Y']
+encode_alphabet="".join(alphabet2)
+
+def decode(text, alphabet, encode_alphabet):
+    original_text=""
+
+    for letter in text:
+        index= encode_alphabet.index(letter)
+        original_text+= alphabet[index]
+    
+    return original_text
+
+def encode(text, alphabet, encode_alphabet):
+    encrypt_text=""
+
+    for letter in text:
+        index= alphabet.index(letter)
+        encrypt_text+= encode_alphabet[index]
+    
+    return encrypt_text
 
 def client_program():
     host = socket.gethostname()  # as both code is running on same pc
