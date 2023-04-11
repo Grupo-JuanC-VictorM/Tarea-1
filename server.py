8b566ee1e0463a2065ca2ecdfcccdd36a234b6f3
Juan Cáceres <juan.caceresb@sansano.usm.cl>
2023-04-10 22:17:19 -0400

    Codificación/decodificación

--- server.py
+++ server.py
@@ -1,5 +1,35 @@
 import socket
+import string
 
+# Alfabeto para decodificar/codificar:
+'''
+ ['{', 'C', 'g', 'r', '/', 'U', 'a', 'W', 'z', 'L', ',', '$', 'n', 'V', 'A', 'k', 'X', 
+ '`', 'w', ')', '(', 'J', '<', 'E', 'B', '_', '#', 'u', 'P', 'p', 'I', '[', 'M', ']', 
+ 'Q', '"', 'l', '|', 'T', '}', '!', 'j', 'm', '=', 'F', '\\', '+', 'o', '.', 'y', '-', 
+ 'G', 'q', '?', '>', ';', 'O', 'f', '*', ' ', 'S', 'R', ':', 't', '^', "'", 'b', 's', 
+ 'c', 'Z', 'd', 'x', '~', '@', 'K', 'h', 'N', 'D', 'e', '%', 'v', '&', 'H', 'i', 'Y']
+'''
+alphabet = "".join([" ",string.ascii_letters,string.punctuation])
+alphabet2=['{', 'C', 'g', 'r', '/', 'U', 'a', 'W', 'z', 'L', ',', '$', 'n', 'V', 'A', 'k', 'X', '`', 'w', ')', '(', 'J', '<', 'E', 'B', '_', '#', 'u', 'P', 'p', 'I', '[', 'M', ']', 'Q', '"', 'l', '|', 'T', '}', '!', 'j', 'm', '=', 'F', '\\', '+', 'o', '.', 'y', '-', 'G', 'q', '?', '>', ';', 'O', 'f', '*', ' ', 'S', 'R', ':', 't', '^', "'", 'b', 's', 'c', 'Z', 'd', 'x', '~', '@', 'K', 'h', 'N', 'D', 'e', '%', 'v', '&', 'H', 'i', 'Y']
+encode_alphabet="".join(alphabet2)
+
+def decrypt(text, alphabet, encode_alphabet):
+    original_text=""
+
+    for letter in text:
+        index= encode_alphabet.index(letter)
+        original_text+= alphabet[index]
+    
+    return original_text
+
+def encrypt(text, alphabet, encode_alphabet):
+    encrypt_text=""
+
+    for letter in text:
+        index= alphabet.index(letter)
+        encrypt_text+= encode_alphabet[index]
+    
+    return encrypt_text
 
 def server_program():
     # get the hostname
@@ -16,13 +46,13 @@ def server_program():
     print("Connection from: " + str(address))
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
+        data = decrypt(conn.recv(1024).decode(),alphabet,encode_alphabet)
         if not data:
             # if data is not received break
             break
         print("from connected user: " + str(data))
         data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+        conn.send(encrypt(data,alphabet,encode_alphabet).encode())  # send data to the client
 
     conn.close()  # close the connection
 
